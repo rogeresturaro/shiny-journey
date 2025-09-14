4ebe4b13c88015d895a0761e2dfb62405d7b162b
rogeresturaro <rogeresturaro@gmail.com>
2025-09-14 00:02:28 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
+import sys
+import os
 import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from main import soma, subtracao, multiplicacao, divisao
+
 
 def test_soma():
     assert soma(2, 3) == 5
@@ -17,3 +23,4 @@ def test_divisao():
     assert divisao(10, 2) == 5
     with pytest.raises(ValueError):
         divisao(10, 0)
+
